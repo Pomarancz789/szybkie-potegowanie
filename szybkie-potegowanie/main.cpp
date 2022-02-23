67d0b721feb912c8026a7d71416cfc69d4d823bc
Pomarancz789 <granat-02krawcy@icloud.com>
2022-02-23 10:17:17 +0100

    Initial Commit

---
+++ szybkie-potegowanie/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  szybkie-potegowanie
+//
+//  Created by Jakub Wtorkiewicz on 23/02/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
