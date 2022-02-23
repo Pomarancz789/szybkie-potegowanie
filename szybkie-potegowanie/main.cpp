e68cca1a306ce277496ee34223387d78e0d6d9f7
Pomarancz789 <granat-02krawcy@icloud.com>
2022-02-23 10:38:39 +0100

    popełniam

--- szybkie-potegowanie/main.cpp
+++ szybkie-potegowanie/main.cpp
@@ -1,14 +1,28 @@
-//
-//  main.cpp
-//  szybkie-potegowanie
-//
-//  Created by Jakub Wtorkiewicz on 23/02/2022.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+long long potega (int a, int b)
+{
+    if (b==0)
+        return 1;
+    else
+        if (b%2==0)
+            return potega(a, b/2)*potega(a, b/2);
+        else
+            return potega(a, b-1)*a;
+}
+
+
+int main() {
+    int a=0;
+    int b=0;
+    
+    cout << "podaj podstawe" << endl;
+    cin >> a;
+    cout << "podaj wykladnik" << endl;
+    cin >> b;
+    
+    cout << potega(a, b) << endl;
+    
     return 0;
 }
